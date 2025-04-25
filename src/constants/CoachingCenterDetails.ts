978ceba700ee4fa9d32b955d55c14bdb74e1398e
monster-1951 <shivashankarsince19@gmail.com>
2025-04-25 13:26:23 +0530

    2...

--- src/constants/CoachingCenterDetails.ts
+++ src/constants/CoachingCenterDetails.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-export const COACHING_NAME = "Bright Future Academy" 
-=======
-export const COACHING_NAME = "Our Coaching Center" 
->>>>>>> 50eef3898ad1eb1b8989ffd1a20e9a14333ef815
+export const COACHING_NAME = "Bright Future Academy"
 export const COURSES = ['IIT-JEE', 'NEET', 'SSC', 'Banking','UPSC']
 export const BRANCHES = ['ECIL , Hyderabad', 'Ameerpet , Hyderabad', 'Koti , Hyderabad']
