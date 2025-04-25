50eef3898ad1eb1b8989ffd1a20e9a14333ef815
Shiva Shankar <146169888+monster-1951@users.noreply.github.com>
2025-04-25 13:08:08 +0530

    Update CoachingCenterDetails.ts

--- src/constants/CoachingCenterDetails.ts
+++ src/constants/CoachingCenterDetails.ts
@@ -1,3 +1,3 @@
-export const COACHING_NAME = "Brainwiz" 
+export const COACHING_NAME = "Our Coaching Center" 
 export const COURSES = ['IIT-JEE', 'NEET', 'SSC', 'Banking','UPSC']
 export const BRANCHES = ['ECIL , Hyderabad', 'Ameerpet , Hyderabad', 'Koti , Hyderabad']
